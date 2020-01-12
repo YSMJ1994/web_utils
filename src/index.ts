324f97568371af159178d732374922c7282d20fa
zhaopengcheng <710411486@qq.com>
2020-01-12 16:35:45 +0800

    add useScrollLock and add useScrollInOut to index

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,7 @@ export { default as useTimeout } from './react/hooks/useTimeout';
 export { default as useInterval } from './react/hooks/useInterval';
 export { default as useFocus } from './react/hooks/useFocus';
 export { default as useClickOutside } from './react/hooks/useClickOutside';
+export { default as useScrollInOut } from './react/hooks/useScrollInOut';
+export { default as useScrollLock } from './react/hooks/useScrollLock';
 // hoc
 export { default as combineProvider } from './react/hoc/combineProvider';
